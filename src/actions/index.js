e5e9fa0e730eaf99dde46876562fdeea0cd7602a
Raidon Amakobe <Raidonmgmt@gmail.com>
2019-05-15 19:11:54 -0400

    switched exports under action

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,10 +2,6 @@
 import axios from "axios";
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
-export const FETCHING = "FETCHING";
-export const SUCCESS = "SUCCESS";
-export const FAILURE = "FAILURE";
-
 export function fetching() {
     return dispatch => {
         dispatch({ type: FETCHING });
@@ -25,6 +21,10 @@ export function fetching() {
             });
     }
 }
+export const FETCHING = "FETCHING";
+export const SUCCESS = "SUCCESS";
+export const FAILURE = "FAILURE";
+
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
