9518aca7230706d187a8b189cf919e2ff013ebc3
Raidon Amakobe <Raidonmgmt@gmail.com>
2019-05-15 19:10:52 -0400

    built out action which sends axios request

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,25 @@ export const FETCHING = "FETCHING";
 export const SUCCESS = "SUCCESS";
 export const FAILURE = "FAILURE";
 
-export function fetching()
+export function fetching() {
+    return dispatch => {
+        dispatch({ type: FETCHING });
+        axios
+            .get('https://swapi.co/api/people/')
+            .then(response => {
+                dispatch({
+                    type: SUCCESS,
+                    payload: response.data.results
+                });
+            })
+            .catch(error => {
+                dispatch({
+                    type: FAILURE,
+                    payload: error
+                });
+            });
+    }
+}
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
